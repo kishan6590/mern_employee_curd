1df3b988d08bea4feaa2ca97612e3a4007145441
Kishan Thapa <kishanthapa4045@gmail.com>
2025-05-31 01:02:39 +0530

    ui fix

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <Navigation />
-      <div className="pageStatus">Dashboard</div>
+      <div className="pageStatus">Home</div>
       <h2 className="welcomeLine"> Home Page</h2>
     </>
   );
